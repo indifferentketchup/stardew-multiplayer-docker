522a8c1ce18cec9296b37b0414396ed5a2e582a0
indifferentketchup <159190319+indifferentketchup@users.noreply.github.com>
2024-02-29 18:27:56 -0600

    Add files via upload

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 export HOME=/config
 
+# start pulseaudio
+pulseaudio -D --exit-idle-time=-1
+
+# create a virtual speaker output, otherwise SpaceCore will crash when trying
+# to find output device, and Stardew Valley Expanded will not load
+pactl load-module module-null-sink \
+    sink_name=SpeakerOutput \
+    sink_properties=device.description="dummy"
+    
 for modPath in /data/Stardew/Stardew\ Valley/Mods/*/
 do
   mod=$(basename "$modPath")
